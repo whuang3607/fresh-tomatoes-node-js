cd02fd792898b5dc24a7423a791e6e1f9972013b
Chirag Malhotra <cmalhotradelhi@gmail.com>
2024-04-21 15:08:47 -0400

    Fixed adding movies

--- testMovies/movie_dao.js
+++ testMovies/movie_dao.js
@@ -1,3 +1,4 @@
-import testMovies from "./movie_model.js";
+import testMovies from './movie_model.js';
 
 export const findMovieById = (movieId) => testMovies.findOne({ id: movieId });
+export const createMovie = (movie) => testMovies.create(movie);
--- testMovies/movie_routes.js
+++ testMovies/movie_routes.js
@@ -1,13 +1,17 @@
-import * as movieDao from "./movie_dao.js";
+import * as movieDao from './movie_dao.js';
 
 function testMovieRoutes(app) {
   const findMovieById = async (req, res) => {
     const movie = await movieDao.findMovieById(req.params.movieId);
     res.json(movie);
   };
+  const createMovie = async (req, res) => {
+    const movie = await movieDao.createMovie(req.body);
+    res.json(movie);
+  };
 
-
-  app.get("/api/testMovies/:movieId", findMovieById);
+  app.post('/api/moviesByAdmin', createMovie);
+  app.get('/api/testMovies/:movieId', findMovieById);
 }
 
 export default testMovieRoutes;
